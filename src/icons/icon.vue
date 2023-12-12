bcc282aed85cefa7156c62bc32e2ad0aabdc1858
Dmitriy Stepanov <autistik430@gmail.com>
2023-12-13 00:45:27 +0500

    #

---
+++ src/icons/icon.vue
@@ -0,0 +1,12 @@
+<template>
+    <home />
+</template>
+
+<script>
+import { home } from "./variants";
+export default {
+    name: "Icon",
+    components: {home}
+}
+
+</script>
---
+++ src/pages/feeds/feeds.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="topline">
+        <topline>
+           <template #headline>
+                <h1>Header</h1>
+           </template> 
+           <template #content>
+            <h2> stories</h2>
+           </template>
+        </topline>
+    </div>
+</template>
+
+<script>
+import { topline  } from '../../components/topline';
+import { icon } from "../../icons";
+export default {
+    name: 'feeds',
+    components: {
+        topline,
+        icon
+    }
+}
+</script>
